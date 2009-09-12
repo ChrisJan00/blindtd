c7fb65dc0b111a8522b28a87151700f96a9ddcc8
Christiaan Janssen <cjan_ynzn@yahoo.es>
2009-09-12 19:38:56 +0200

    Template files

---
+++ game.lua
@@ -0,0 +1,49 @@
+
+
+-- Blind Tower Defense (temporal name)
+-- Copyright 2009 Iwan Gabovitch, Christiaan Janssen, September 2009
+-- 
+-- This file is part of Blind Tower Defense
+-- 
+--     Blind Tower Defense is free software: you can redistribute it and/or modify
+--     it under the terms of the GNU General Public License as published by
+--     the Free Software Foundation, either version 3 of the License, or
+--     (at your option) any later version.
+-- 
+--     Blind Tower Defense is distributed in the hope that it will be useful,
+--     but WITHOUT ANY WARRANTY; without even the implied warranty of
+--     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--     GNU General Public License for more details.
+-- 
+--     You should have received a copy of the GNU General Public License
+--     along with Blind Tower Defense  If not, see <http://www.gnu.org/licenses/>.
+
+Game = {}
+
+function Game.load()
+end
+
+function Game.update(dt)
+end
+
+
+function Game.draw()
+end
+
+
+function Game.keypressed(key)
+end
+
+
+function Game.keyreleased(key)
+end
+
+
+function Game.mousepressed(x, y, button)
+end
+
+
+
+function Game.mousereleased(x, y, button)
+end
+
---
+++ main.lua
@@ -0,0 +1,79 @@
+-- Blind Tower Defense (temporal name)
+-- Copyright 2009 Iwan Gabovitch, Christiaan Janssen, September 2009
+-- 
+-- This file is part of Blind Tower Defense
+-- 
+--     Blind Tower Defense is free software: you can redistribute it and/or modify
+--     it under the terms of the GNU General Public License as published by
+--     the Free Software Foundation, either version 3 of the License, or
+--     (at your option) any later version.
+-- 
+--     Blind Tower Defense is distributed in the hope that it will be useful,
+--     but WITHOUT ANY WARRANTY; without even the implied warranty of
+--     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--     GNU General Public License for more details.
+-- 
+--     You should have received a copy of the GNU General Public License
+--     along with Blind Tower Defense  If not, see <http://www.gnu.org/licenses/>.
+
+function load()
+
+	-- Dependencies
+	love.filesystem.require("game.lua")
+
+	-- Initialization
+	start_time = love.timer.getTime()
+
+	math.randomseed(os.time())
+
+	-- Init graphics mode
+       screensize = { 640, 480 }
+	if not love.graphics.setMode( screensize[1], screensize[2], false, true, 0 ) then
+		love.system.exit()
+	end
+
+	-- Audio system
+	love.audio.setChannels(16)
+	love.audio.setVolume(.3)
+
+	Game.load()
+
+end
+
+function update(dt)
+
+	Game.update(dt)
+
+end
+
+
+function draw()
+	Game.draw()
+end
+
+
+function keypressed(key)
+	Game.keypressed(key)
+end
+
+
+function keyreleased(key)
+	Game.keyreleased(key)
+end
+
+
+function mousepressed(x, y, button)
+	Game.mousepressed(x,y,button)
+
+end
+
+
+
+function mousereleased(x, y, button)
+
+	Game.mousereleased(x,y, button)
+
+end
+
+
+
