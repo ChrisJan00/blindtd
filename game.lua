caee277d198b4bd310ad566e35d5d90b78c951a2
Christiaan Janssen <cjan_ynzn@yahoo.es>
2009-09-12 21:52:37 +0200

    can switch walls and doors with the mouse, shift deactivates corridor

--- game.lua
+++ game.lua
@@ -21,12 +21,12 @@
 Game = {}
 
 function Game.load()
-love.filesystem.require("map.lua")
-examplemap()
+	love.filesystem.require("map.lua")
+	examplemap()
 
-love.graphics.setBackgroundColor(0,0,48)
-love.graphics.setColor(0,160,160)
-love.graphics.setLine(2)
+	love.graphics.setBackgroundColor(0,0,48)
+	love.graphics.setColor(0,160,160)
+	love.graphics.setLine(2)
 end
 
 function Game.update(dt)
@@ -39,6 +39,9 @@ end
 
 
 function Game.keypressed(key)
+	if key == love.key_escape then
+		love.system.exit()
+	end
 end
 
 
@@ -47,8 +50,158 @@ end
 
 
 function Game.mousepressed(x, y, button)
-end
+	-- find where
+	-- 1. which cell
+--~ 	local dx = math.floor(screensize[1]/Map.hcells)
+--~ 	local dy = math.floor(screensize[2]/Map.vcells)
+--~ 	-- force square
+--~ 	dx = dy 
+	local dx,dy = Map.cell_w,Map.cell_h
+	local cx = math.floor(x/dx)+1
+	local cy = math.floor(y/dy)+1
+	local lx = x%dx
+	local ly = y%dy
+	
+	
 
+	if cx>Map.hcells or cy>Map.vcells then
+		-- outside of map: ignore
+		return
+	end
+	
+	if love.keyboard.isDown(love.key_lshift) and button == love.mouse_left then
+		Map[cx][cy].corridor = not Map[cx][cy].corridor
+		return
+	end
+	
+	if lx>ly and (dx-lx)>ly then -- upper wall
+		-- switch wall
+		if button == love.mouse_left then
+			if Map[cx][cy].u == 0 then
+				Map[cx][cy].u = 1
+				if cy>1 then
+					Map[cx][cy-1].d = 1
+				end
+			else
+				Map[cx][cy].u = 0
+				if cy>1 then
+					Map[cx][cy-1].d = 0
+				end
+			end
+		end
+		
+		-- switch door
+		if button == love.mouse_right then
+			if Map[cx][cy].u == 2 then
+				Map[cx][cy].u = 3
+				if cy>1 then
+					Map[cx][cy-1].d = 3
+				end
+			else
+				Map[cx][cy].u = 2
+				if cy>1 then
+					Map[cx][cy-1].d = 2
+				end
+			end
+		end
+	end
+	
+	
+	if lx>ly and (dx-lx)<=ly then -- right wall
+		-- switch wall
+		if button == love.mouse_left then
+			if Map[cx][cy].r == 0 then
+				Map[cx][cy].r = 1
+				if cx<Map.hcells then
+					Map[cx+1][cy].l = 1
+				end
+			else
+				Map[cx][cy].r = 0
+				if cx<Map.hcells then
+					Map[cx+1][cy].l = 0
+				end
+			end
+		end
+		
+		-- switch door
+		if button == love.mouse_right then
+			if Map[cx][cy].r == 2 then
+				Map[cx][cy].r = 3
+				if cx<Map.hcells then
+					Map[cx+1][cy].l = 3
+				end
+			else
+				Map[cx][cy].r = 2
+				if cx<Map.hcells then
+					Map[cx+1][cy].l = 2
+				end
+			end
+		end
+	end
+	
+	if lx<ly and  lx<=(dy-ly) then -- left wall
+		-- switch wall
+		if button == love.mouse_left then
+			if Map[cx][cy].l == 0 then
+				Map[cx][cy].l = 1
+				if cx>1 then
+					Map[cx-1][cy].r = 1
+				end
+			else
+				Map[cx][cy].l = 0
+				if cx>1 then
+					Map[cx-1][cy].r = 0
+				end
+			end
+		end
+		
+		-- switch door
+		if button == love.mouse_right then
+			if Map[cx][cy].l == 2 then
+				Map[cx][cy].l = 3
+				if cx>1 then
+					Map[cx-1][cy].r = 3
+				end
+			else
+				Map[cx][cy].l = 2
+				if cx>1 then
+					Map[cx-1][cy].r = 2
+				end
+			end
+		end
+	end
+	if lx<ly and lx>(dy-ly) then -- down wall
+		-- switch wall
+		if button == love.mouse_left then
+			if Map[cx][cy].d == 0 then
+				Map[cx][cy].d = 1
+				if cy<Map.vcells then
+					Map[cx][cy+1].u = 1
+				end
+			else
+				Map[cx][cy].d = 0
+				if cy<Map.vcells then
+					Map[cx][cy+1].u = 0
+				end
+			end
+		end
+		
+		-- switch door
+		if button == love.mouse_right then
+			if Map[cx][cy].d == 2 then
+				Map[cx][cy].d = 3
+				if cy<Map.vcells then
+					Map[cx][cy+1].u = 3
+				end
+			else
+				Map[cx][cy].d = 2
+				if cy<Map.vcells then
+					Map[cx][cy+1].u = 2
+				end
+			end
+		end
+	end
+end
 
 
 function Game.mousereleased(x, y, button)
--- map.lua
+++ map.lua
@@ -1,7 +1,32 @@
+
+-- Blind Tower Defense (temporal name)
+-- Copyright 2009 Iwan Gabovitch, Christiaan Janssen, September 2009
+-- 
+-- This file is part of Blind Tower Defense
+-- 
+--     Blind Tower Defense is free software: you can redistribute it and/or modify
+--     it under the terms of the GNU General Public License as published by
+--     the Free Software Foundation, either version 3 of the License, or
+--     (at your option) any later version.
+-- 
+--     Blind Tower Defense is distributed in the hope that it will be useful,
+--     but WITHOUT ANY WARRANTY; without even the implied warranty of
+--     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--     GNU General Public License for more details.
+-- 
+--     You should have received a copy of the GNU General Public License
+--     along with Blind Tower Defense  If not, see <http://www.gnu.org/licenses/>.
+
+
 Map={}
 	
 Map.hcells = 20
 Map.vcells = 20
+Map.cell_w = math.floor(screensize[1]/Map.hcells)
+Map.cell_h= math.floor(screensize[2]/Map.vcells)
+-- force square
+Map.cell_w = Map.cell_h 
+
 
 function examplemap()
 local i,j
@@ -24,23 +49,28 @@ for i=1,Map.hcells do
   end
 end
 
-	Map[5][5].u = 1
-	Map[5][4].d = 1
-	Map[6][6].d = 2
-	Map[6][7].u = 2
-	Map[7][7].r = 3
-	Map[8][7].l = 3
-	Map[8][8].l = 4
-	Map[8][7].r = 4
+--~ 	Map[5][5].u = 1
+--~ 	Map[5][4].d = 1
+--~ 	Map[6][6].d = 2
+--~ 	Map[6][7].u = 2
+--~ 	Map[7][7].r = 3
+--~ 	Map[8][7].l = 3
+--~ 	Map[8][8].l = 4
+--~ 	Map[8][7].r = 4
 end
 
 function drawmap(map)
 	local i,j
-	local dx=screensize[1]/Map.hcells
-	local dy=screensize[2]/Map.vcells
-	dx = dy
+--~ 	local dx=screensize[1]/Map.hcells
+--~ 	local dy=screensize[2]/Map.vcells
+	local dx,dy = Map.cell_w,Map.cell_h
+--~ 	dx = dy
 	for i=1,Map.hcells do
 		for j=1,Map.vcells do
+			if  not map[i][j].corridor then
+				love.graphics.setColor(0,48,48)
+				love.graphics.rectangle( love.draw_fill , (i-1)*dx+1,(j-1)*dy+1,dx-1,dy-1 ) 
+			end
 			if map[i][j].corridor then
 				-- walls
 				if map[i][j].u==0 then
