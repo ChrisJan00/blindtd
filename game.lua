fd2999159640fdc75fc1b987a509a30a8e2bfc7a
ChrisJan00 <Christiaan@.(none)>
2010-01-31 19:08:26 +0100

    All rooms have doors and the guy actually walks

--- game.lua
+++ game.lua
@@ -2,19 +2,19 @@
 
 -- Blind Tower Defense (temporal name)
 -- Copyright 2009 Iwan Gabovitch, Christiaan Janssen, September 2009
--- 
+--
 -- This file is part of Blind Tower Defense
--- 
+--
 --     Blind Tower Defense is free software: you can redistribute it and/or modify
 --     it under the terms of the GNU General Public License as published by
 --     the Free Software Foundation, either version 3 of the License, or
 --     (at your option) any later version.
--- 
+--
 --     Blind Tower Defense is distributed in the hope that it will be useful,
 --     but WITHOUT ANY WARRANTY; without even the implied warranty of
 --     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 --     GNU General Public License for more details.
--- 
+--
 --     You should have received a copy of the GNU General Public License
 --     along with Blind Tower Defense  If not, see <http://www.gnu.org/licenses/>.
 
@@ -30,32 +30,65 @@ function Game.load()
 	love.graphics.setBackgroundColor(0,0,48)
 	love.graphics.setColor(0,160,160)
 	love.graphics.setLine(2)
-	
+
 	-- 1 edit
 	-- 2 move guy
 	gamemode = 2
+	step_counter = 0
+	step_size = 0.08
 end
 
 function Game.update(dt)
+	local do_step = false
+	local nsteps = 1
+	if step_counter>0 then
+		step_counter = step_counter - dt
+	end
+	if step_counter<=0 then
+		do_step = true
+		nsteps = math.floor(math.abs(step_counter/step_size))+1
+		step_counter = step_size
+	end
+
+	if mypath and do_step then
+		if table.getn(mypath)>0 then
+			if nsteps>table.getn(mypath) then nsteps=table.getn(mypath) end
+			currentpos = mypath[nsteps]
+			local i
+			for i=1,nsteps do
+				table.remove(mypath,1)
+			end
+		end
+	end
 end
 
 
 function Game.draw()
 	drawmap(Map)
-	drawpath(mypath)
-	drawscanlines()
+	--drawpath(mypath)
+	drawchar(currentpos)
+--~ 	drawscanlines()
+end
+
+function drawchar( c )
+
+	if not c then return end
+	local dx,dy = Cell.width,Cell.height
+
+	love.graphics.setColor(188,168,0)
+	love.graphics.rectangle( love.draw_fill , (c[1]-1)*dx+1,(c[2]-1)*dy+1,dx-1,dy-1 )
 end
 
 function drawpath( p )
 	local i,v
 	if not p then return end
 	local dx,dy = Cell.width,Cell.height
-	for i,v in ipairs(p) do 
-		love.graphics.setColor(188,168,0)
-		love.graphics.rectangle( love.draw_fill , (v[1]-1)*dx+1,(v[2]-1)*dy+1,dx-1,dy-1 ) 
+	for i,v in ipairs(p) do
+		love.graphics.setColor(188,168,i*255/table.getn(p))
+		love.graphics.rectangle( love.draw_fill , (v[1]-1)*dx+1,(v[2]-1)*dy+1,dx-1,dy-1 )
 	end
 end
- 
+
 function drawscanlines()
 	local i
 	local shalf = math.floor( screensize[2]/2)
@@ -71,15 +104,15 @@ function Game.keypressed(key)
 	if key == love.key_escape then
 		love.system.exit()
 	end
-	
+
 	if key == love.key_s then
 		saveMap( Map )
 	end
-	
+
 	if key == love.key_l then
 		openMap( maps.lines )
 	end
-	
+
 	if key == love.key_e then
 		gamemode = gamemode + 1
 		if gamemode > 2 then gamemode = 1 end
@@ -97,27 +130,27 @@ function Game.mousepressed(x, y, button)
 --~ 	local dx = math.floor(screensize[1]/Map.hcells)
 --~ 	local dy = math.floor(screensize[2]/Map.vcells)
 --~ 	-- force square
---~ 	dx = dy 
+--~ 	dx = dy
 	local dx,dy = Cell.width,Cell.height
 	local cx = math.floor(x/dx)+1
 	local cy = math.floor(y/dy)+1
 	local lx = x%dx
 	local ly = y%dy
-	
-	
+
+
 
 	if cx>Map.hcells or cy>Map.vcells then
 		-- outside of map: ignore
 		return
 	end
-	
+
 	if gamemode == 1 then
-	
+
 		if love.keyboard.isDown(love.key_lshift) and button == love.mouse_left then
 			Map[cx][cy].corridor = not Map[cx][cy].corridor
 			return
 		end
-		
+
 		if lx>ly and (dx-lx)>ly then -- upper wall
 			-- switch wall
 			if button == love.mouse_left then
@@ -133,7 +166,7 @@ function Game.mousepressed(x, y, button)
 					end
 				end
 			end
-			
+
 			-- switch door
 			if button == love.mouse_right then
 				if Map[cx][cy].u == 2 then
@@ -149,8 +182,8 @@ function Game.mousepressed(x, y, button)
 				end
 			end
 		end
-		
-		
+
+
 		if lx>ly and (dx-lx)<=ly then -- right wall
 			-- switch wall
 			if button == love.mouse_left then
@@ -166,7 +199,7 @@ function Game.mousepressed(x, y, button)
 					end
 				end
 			end
-			
+
 			-- switch door
 			if button == love.mouse_right then
 				if Map[cx][cy].r == 2 then
@@ -182,7 +215,7 @@ function Game.mousepressed(x, y, button)
 				end
 			end
 		end
-		
+
 		if lx<ly and  lx<=(dy-ly) then -- left wall
 			-- switch wall
 			if button == love.mouse_left then
@@ -198,7 +231,7 @@ function Game.mousepressed(x, y, button)
 					end
 				end
 			end
-			
+
 			-- switch door
 			if button == love.mouse_right then
 				if Map[cx][cy].l == 2 then
@@ -229,7 +262,7 @@ function Game.mousepressed(x, y, button)
 					end
 				end
 			end
-			
+
 			-- switch door
 			if button == love.mouse_right then
 				if Map[cx][cy].d == 2 then
@@ -245,13 +278,13 @@ function Game.mousepressed(x, y, button)
 				end
 			end
 		end
-	
+
 	end
-	
+
 	if gamemode == 2 and Map[cx][cy].corridor and button == love.mouse_left then
 		if not currentpos then currentpos = {cx,cy} end
 		mypath = find_route( currentpos, {cx,cy}, Map )
-		currentpos = {cx,cy}
+		--currentpos = {cx,cy}
 	end
 end
 
--- generator.lua
+++ generator.lua
@@ -1,18 +1,18 @@
 -- Blind Tower Defense (temporal name)
 -- Copyright 2009 Iwan Gabovitch, Christiaan Janssen, September 2009
--- 
+--
 -- This file is part of Blind Tower Defense
--- 
+--
 --     Blind Tower Defense is free software: you can redistribute it and/or modify
 --     it under the terms of the GNU General Public License as published by
 --     the Free Software Foundation, either version 3 of the License, or
 --     (at your option) any later version.
--- 
+--
 --     Blind Tower Defense is distributed in the hope that it will be useful,
 --     but WITHOUT ANY WARRANTY; without even the implied warranty of
 --     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 --     GNU General Public License for more details.
--- 
+--
 --     You should have received a copy of the GNU General Public License
 --     along with Blind Tower Defense  If not, see <http://www.gnu.org/licenses/>.
 
@@ -21,18 +21,18 @@
 function generateMapWRooms()
 	local map={}
 	local marks={}
-	
+
 	local i,j
 
 	local room_prob = 0.04
 	local step_door = 12
-	
+
 	-- closed: (cannot go through) 0
 	-- open: (can go through) 1
 	-- door open: 2
 	-- door closed: 3
 	-- door blocked: 4
-	
+
 --~ 	map.hcells = 20
 --~ 	map.vcells = 20
 	map.hcells = Map.hcells
@@ -41,7 +41,7 @@ function generateMapWRooms()
 	for i=1,map.hcells do
 		map[i]={}
 		marks[i]={}
-		
+
 		for j=1,map.vcells do
 			map[i][j]={
 				u = 0,
@@ -53,24 +53,24 @@ function generateMapWRooms()
 			marks[i][j]=0
 		end
 	end
-	
+
 	local x,y = math.random(map.hcells),math.random(map.vcells)
 	local stack={}
 	local ndx
 	stack[1] = {x,y,0,0}
-	
-	
+
+
 	-- directions: 1 up, 2 down, 3 left, 4 right
-	
+
 	while table.getn(stack) > 0 do
 		ndx = math.random(table.getn(stack))
 		x,y = stack[ndx][1],stack[ndx][2]
 		local fromdir = stack[ndx][3]
 		local step=stack[ndx][4]
 		table.remove(stack, ndx)
-		
-		if marks[x][y]<2 then 
-		
+
+		if marks[x][y]<2 then
+
 			if (math.random()<room_prob) then
 			-- make room
 				local roombox = {x,y,x,y}
@@ -78,22 +78,24 @@ function generateMapWRooms()
 				local checkroom = true
 				local maxW,maxH = 5,5
 				local taboo={false,false,false,false}
-				
+
 				marks[x][y]=0
-				
+
+
+
 				-- grow room
 				while checkroom and roombox[3]-roombox[1]+1<maxW and roombox[4]-roombox[2]+1<maxH do
 					-- choose direction to grow
 					local dirgrow = math.random(4)
-					if (taboo[1] and taboo[2] and taboo[3] and taboo[4]) then 
-						checkroom = false 
+					if (taboo[1] and taboo[2] and taboo[3] and taboo[4]) then
+						checkroom = false
 						break
 					end
 
 					while taboo[dirgrow] do
 						dirgrow = dirgrow%4+1
 					end
-					
+
 					-- grow
 					if dirgrow==1 and roombox[1]>1 then roomcandidate[1]=roombox[1]-1 end
 					if dirgrow==2 and roombox[2]>1 then roomcandidate[2]=roombox[2]-1 end
@@ -102,13 +104,13 @@ function generateMapWRooms()
 					taboo[dirgrow]=true
 					-- check
 					checkroom = true
-					for i=roomcandidate[1],roomcandidate[3] do 
-						for j=roomcandidate[2],roomcandidate[4] do 
+					for i=roomcandidate[1],roomcandidate[3] do
+						for j=roomcandidate[2],roomcandidate[4] do
 							if marks[i][j] >0 then checkroom = false end
 						end
 					end
 					if not checkroom then
-						-- reject						
+						-- reject
 						roomcandidate[dirgrow] = roombox[dirgrow]
 						if not (taboo[1] and taboo[2] and taboo[3] and taboo[4]) then checkroom=true end
 					elseif roombox[dirgrow] ~= roomcandidate[dirgrow] then
@@ -117,9 +119,9 @@ function generateMapWRooms()
 						roombox[dirgrow] = roomcandidate[dirgrow]
 					end
 				end
-				
+
 				-- now that we have a room, place it in the map
-				for i=roombox[1],roombox[3] do 
+				for i=roombox[1],roombox[3] do
 					for j=roombox[2],roombox[4] do
 						marks[i][j]=3
 						map[i][j].corridor=true
@@ -133,47 +135,78 @@ function generateMapWRooms()
 						if i<map.hcells then map[i+1][j].l=1 end
 					end
 				end
-				
-				
-				
+
+				-- open
+
+
+			map[x][y].corridor = true
+			if fromdir==1 then
+
+				map[x][y-1].d = 2
+				map[x][y].u = 2
+				marks[x][y-1]=3
+			end
+			if fromdir ==2 then
+
+				map[x][y+1].u=2
+				map[x][y].d=2
+				marks[x][y+1]=3
+			end
+			if fromdir == 3 then
+
+				map[x-1][y].r = 2
+				map[x][y].l = 2
+				marks[x-1][y]=3
+			end
+			if fromdir == 4 then
+
+				map[x+1][y].l = 2
+				map[x][y].r = 2
+				marks[x+1][y]=3
+			end
+
 				-- horizontal walls
-				for i=roombox[1],roombox[3] do 
+				for i=roombox[1],roombox[3] do
 					local top,bot = roombox[2],roombox[4]
-					if top>1 and not map[i][top - 1].corridor then 
-						map[i][top].u = 0 
+					marks[i][top]=5
+					marks[i][bot]=5
+					if top>1 and not map[i][top - 1].corridor then
+						map[i][top].u = 0
 						map[i][top-1].d = 0
-						if marks[i][top-1]==0 then 
-							marks[i][top-1]=1 
+						if marks[i][top-1]==0 then
+							marks[i][top-1]=1
 							table.insert(stack,{i,top-1,2,step+1})
-						elseif marks[i][top-1]==1 then 
-							marks[i][top-1]=2 
+						elseif marks[i][top-1]==1 then
+							marks[i][top-1]=2
 						end
 					elseif top==1 then map[i][top].u = 0 end
-					if bot<map.vcells and not map[i][bot + 1].corridor then 
-						map[i][bot].d = 0 
+					if bot<map.vcells and not map[i][bot + 1].corridor then
+						map[i][bot].d = 0
 						map[i][bot+1].u = 0
-						if marks[i][bot+1]==0 then 
-							marks[i][bot+1]=1 
+						if marks[i][bot+1]==0 then
+							marks[i][bot+1]=1
 							table.insert(stack,{i,bot+1,1,step+1})
 						elseif  marks[i][bot+1]==1 then marks[i][bot+1]=2 end
 					elseif bot==map.vcells then map[i][bot].d = 0 end
 				end
-			
+
 				-- vertical walls
-				for j=roombox[2],roombox[4] do 
+				for j=roombox[2],roombox[4] do
 					local lt,rt = roombox[1],roombox[3]
-					if lt>1 and not map[lt - 1][j].corridor then 
-						map[lt][j].l = 0 
+					marks[lt][j]=5
+					marks[rt][j]=5
+					if lt>1 and not map[lt - 1][j].corridor then
+						map[lt][j].l = 0
 						map[lt-1][j].r = 0
-						if marks[lt-1][j]==0 then 
+						if marks[lt-1][j]==0 then
 							marks[lt-1][j]=1
 							table.insert(stack,{lt-1,j,4,step+1})
 						elseif  marks[lt-1][j]==1 then marks[lt-1][j]=2 end
 					elseif lt==1 then map[lt][j].l = 0 end
-					if rt<map.hcells and not map[rt + 1][j].corridor then 
-						map[rt][j].r = 0 
+					if rt<map.hcells and not map[rt + 1][j].corridor then
+						map[rt][j].r = 0
 						map[rt+1][j].l = 0
-						if marks[rt+1][j]==0 then 
+						if marks[rt+1][j]==0 then
 							marks[rt+1][j]=1
 							table.insert(stack,{rt+1,j,3,step+1})
 						elseif  marks[rt+1][j]==1 then marks[rt+1][j]=2 end
@@ -181,35 +214,39 @@ function generateMapWRooms()
 				end
 
 			else
-			
+
 			marks[x][y]=4
-			
+
 			-- open
 			local doortype = 1
 			if step%step_door == 0 then doortype=2 end
-			
+
 			map[x][y].corridor = true
 			if fromdir==1 then
+				if marks[x][y-1]==5 then doortype=2 else doortype=1 end
 				map[x][y-1].d = doortype
 				map[x][y].u = doortype
 				marks[x][y-1]=3
 			end
 			if fromdir ==2 then
+				if marks[x][y+1]==5 then doortype=2 else doortype=1 end
 				map[x][y+1].u=doortype
 				map[x][y].d=doortype
 				marks[x][y+1]=3
 			end
 			if fromdir == 3 then
+				if marks[x-1][y]==5 then doortype=2 else doortype=1 end
 				map[x-1][y].r = doortype
 				map[x][y].l = doortype
 				marks[x-1][y]=3
 			end
 			if fromdir == 4 then
+				if marks[x+1][y]==5 then doortype=2 else doortype=1 end
 				map[x+1][y].l = doortype
 				map[x][y].r = doortype
 				marks[x+1][y]=3
 			end
-			
+
 			-- push up
 			if y>1 then
 				if marks[x][y-1]==0 then
@@ -219,7 +256,7 @@ function generateMapWRooms()
 					marks[x][y-1]=2
 				end
 			end
-			
+
 			-- push down
 			if y<map.vcells then
 				if marks[x][y+1]==0 then
@@ -229,7 +266,7 @@ function generateMapWRooms()
 					marks[x][y+1]=2
 				end
 			end
-			
+
 			-- push left
 			if x>1 then
 				if marks[x-1][y]==0 then
@@ -239,7 +276,7 @@ function generateMapWRooms()
 					marks[x-1][y]=2
 				end
 			end
-			
+
 			-- push right
 			if x<map.hcells then
 				if marks[x+1][y]==0 then
@@ -252,10 +289,10 @@ function generateMapWRooms()
 			end
 		end
 	end
-	
-	
+
+
 	-- now, avoid dead ends in diagonal with other tunnels
-	
+
 	-- find them all
 	stack={}
 	for i=1,map.hcells do
@@ -265,13 +302,13 @@ function generateMapWRooms()
 			end
 		end
 	end
-	
+
 	-- process them randomly, one at a time
 	while table.getn(stack)>0 do
 		ndx = math.random(table.getn(stack))
 		x,y = stack[ndx][1],stack[ndx][2]
 		table.remove(stack,ndx)
-		
+
 		-- check if it is still a u turn
 		local checksum = 0
 		if map[x][y].u ~= 0 then checksum = checksum + 1 end
@@ -279,7 +316,7 @@ function generateMapWRooms()
 		if map[x][y].l ~= 0 then checksum = checksum + 1 end
 		if map[x][y].r ~= 0 then checksum = checksum + 1 end
 		if map[x][y].corridor and checksum==1 then
-			
+
 		-- up
 		if map[x][y].u~=0 then
 			if y<map.vcells and ((x>1 and map[x-1][y+1].corridor) or (x<map.hcells and map[x+1][y+1].corridor)) then
@@ -293,8 +330,8 @@ function generateMapWRooms()
 				map[x][y+2].u=1
 				map[x][y+1].corridor = true
 			end
-		
-		
+
+
 		-- down
 		elseif map[x][y].d~=0 then
 			if y>1 and ((x>1 and map[x-1][y-1].corridor) or (x<map.hcells and map[x+1][y-1].corridor)) then
@@ -308,8 +345,8 @@ function generateMapWRooms()
 				map[x][y-2].d=1
 				map[x][y-1].corridor = true
 			end
-		
-		
+
+
 		-- left
 		elseif map[x][y].l~=0 then
 			if x<map.hcells and ((y>1 and map[x+1][y-1].corridor) or (y<map.vcells and map[x+1][y+1].corridor)) then
@@ -323,8 +360,8 @@ function generateMapWRooms()
 				map[x+2][y].l=1
 				map[x+1][y].corridor = true
 			end
-	
-		
+
+
 		-- right
 		elseif map[x][y].r~=0 then
 			if x>1 and ((y>1 and map[x-1][y-1].corridor) or (y<map.vcells and map[x-1][y+1].corridor)) then
@@ -339,10 +376,10 @@ function generateMapWRooms()
 				map[x-1][y].corridor = true
 			end
 		end
-		
+
 		end
 	end
-	
+
 	return map
 
 end
@@ -352,7 +389,7 @@ end
 function generateMap()
 	local map={}
 	local marks={}
-	
+
 	local i,j
 
 	-- closed: (cannot go through) 0
@@ -360,14 +397,14 @@ function generateMap()
 	-- door open: 2
 	-- door closed: 3
 	-- door blocked: 4
-	
+
 	map.hcells = 20
 	map.vcells = 20
 
 	for i=1,map.hcells do
 		map[i]={}
 		marks[i]={}
-		
+
 		for j=1,map.vcells do
 			map[i][j]={
 				u = 0,
@@ -379,24 +416,24 @@ function generateMap()
 			marks[i][j]=0
 		end
 	end
-	
+
 	local x,y = math.random(map.hcells),math.random(map.vcells)
 	local stack={}
 	local ndx
 	stack[1] = {x,y,0}
-	
-	
+
+
 	-- directions: 1 up, 2 down, 3 left, 4 right
-	
+
 	while table.getn(stack) > 0 do
 		ndx = math.random(table.getn(stack))
 		x,y = stack[ndx][1],stack[ndx][2]
 		local fromdir = stack[ndx][3]
 		table.remove(stack, ndx)
-		
-		if marks[x][y]~=2 then 
+
+		if marks[x][y]~=2 then
 			marks[x][y]=4
-			
+
 			-- open
 			map[x][y].corridor = true
 			if fromdir==1 then
@@ -419,7 +456,7 @@ function generateMap()
 				map[x][y].r = 1
 				marks[x+1][y]=3
 			end
-			
+
 			-- push up
 			if y>1 then
 				if marks[x][y-1]==0 then
@@ -429,7 +466,7 @@ function generateMap()
 					marks[x][y-1]=2
 				end
 			end
-			
+
 			-- push down
 			if y<map.vcells then
 				if marks[x][y+1]==0 then
@@ -439,7 +476,7 @@ function generateMap()
 					marks[x][y+1]=2
 				end
 			end
-			
+
 			-- push left
 			if x>1 then
 				if marks[x-1][y]==0 then
@@ -449,7 +486,7 @@ function generateMap()
 					marks[x-1][y]=2
 				end
 			end
-			
+
 			-- push right
 			if x<map.hcells then
 				if marks[x+1][y]==0 then
@@ -459,13 +496,13 @@ function generateMap()
 					marks[x+1][y]=2
 				end
 			end
-			
+
 		end
 	end
-	
-	
+
+
 	-- now, avoid dead ends in diagonal with other tunnels
-	
+
 	-- find them all
 	stack={}
 	for i=1,map.hcells do
@@ -475,13 +512,13 @@ function generateMap()
 			end
 		end
 	end
-	
+
 	-- process them randomly, one at a time
 	while table.getn(stack)>0 do
 		ndx = math.random(table.getn(stack))
 		x,y = stack[ndx][1],stack[ndx][2]
 		table.remove(stack,ndx)
-		
+
 		-- check if it is still a u turn
 		local checksum = 0
 		if map[x][y].u ~= 0 then checksum = checksum + 1 end
@@ -489,7 +526,7 @@ function generateMap()
 		if map[x][y].l ~= 0 then checksum = checksum + 1 end
 		if map[x][y].r ~= 0 then checksum = checksum + 1 end
 		if map[x][y].corridor and checksum==1 then
-			
+
 		-- up
 		if map[x][y].u~=0 then
 			if y<map.vcells and ((x>1 and map[x-1][y+1].corridor) or (x<map.hcells and map[x+1][y+1].corridor)) then
@@ -503,8 +540,8 @@ function generateMap()
 				map[x][y+2].u=1
 				map[x][y+1].corridor = true
 			end
-		
-		
+
+
 		-- down
 		elseif map[x][y].d~=0 then
 			if y>1 and ((x>1 and map[x-1][y-1].corridor) or (x<map.hcells and map[x+1][y-1].corridor)) then
@@ -518,8 +555,8 @@ function generateMap()
 				map[x][y-2].d=1
 				map[x][y-1].corridor = true
 			end
-		
-		
+
+
 		-- left
 		elseif map[x][y].l~=0 then
 			if x<map.hcells and ((y>1 and map[x+1][y-1].corridor) or (y<map.vcells and map[x+1][y+1].corridor)) then
@@ -533,8 +570,8 @@ function generateMap()
 				map[x+2][y].l=1
 				map[x+1][y].corridor = true
 			end
-	
-		
+
+
 		-- right
 		elseif map[x][y].r~=0 then
 			if x>1 and ((y>1 and map[x-1][y-1].corridor) or (y<map.vcells and map[x-1][y+1].corridor)) then
@@ -549,10 +586,10 @@ function generateMap()
 				map[x-1][y].corridor = true
 			end
 		end
-		
+
 		end
 	end
-	
+
 	return map
 
 end
