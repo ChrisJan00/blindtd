493064bf80c4930f81b1f1ae0f496ae52d663bd0
ChrisJan00 <Christiaan@.(none)>
2010-02-20 19:55:29 +0100

    Some utility functions with linked lists and a scheduler for long iterative tasks

---
+++ linkedlists.lua
@@ -0,0 +1,294 @@
+-- Blind Tower Defense (temporal name)
+-- Copyright 2009 Iwan Gabovitch, Christiaan Janssen, September 2009
+--
+-- This file is part of Blind Tower Defense
+--
+--     Blind Tower Defense is free software: you can redistribute it and/or modify
+--     it under the terms of the GNU General Public License as published by
+--     the Free Software Foundation, either version 3 of the License, or
+--     (at your option) any later version.
+--
+--     Blind Tower Defense is distributed in the hope that it will be useful,
+--     but WITHOUT ANY WARRANTY; without even the implied warranty of
+--     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--     GNU General Public License for more details.
+--
+--     You should have received a copy of the GNU General Public License
+--     along with Blind Tower Defense  If not, see <http://www.gnu.org/licenses/>.
+
+
+-- Double Linked Lists
+
+List = {}
+
+-- create list
+function List.newList()
+	local list = {}
+	list.first = {next=nil, prev=nil, ref=nil, val=nil}
+	list.last = {next=nil, prev=nil, ref=nil, val=nil}
+	list.current = list.first
+	list.n = 0
+	return list
+end
+
+function List.discard(list)
+	local next = list.first.next
+	local this = list.first
+	while this do
+		this.next = nil
+		this.prev = nil
+		this.ref = nil
+		this.value = nil
+		this = next
+		if this then
+			next = this.next
+		end
+	end
+	list.first = {next=nil, prev=nil, ref=nil, val=nil}
+	list.last = {next=nil, prev=nil, ref=nil, val=nil}
+	list.current = list.first
+	list.n = 0
+end
+
+-- push and pop functions
+function List.pushFront(list, obj, value)
+	if not value then value=0 end
+	local new = { next = nil, prev = nil, ref = obj, val = value }
+	if list.n>0 then
+		list.first.prev = new
+		new.next = list.first
+	else
+		list.last = new
+	end
+
+	list.first = new
+	list.n = list.n + 1
+end
+
+function List.pushBack(list, obj, value)
+	if not value then value=0 end
+	local new = { next = nil, prev = nil, ref = obj, val = value }
+	if list.n>0 then
+		list.last.next = new
+		new.prev = list.last
+	else
+		list.first = new
+	end
+
+	list.last = new
+	list.n = list.n + 1
+end
+
+function List.popFront(list)
+	if list.n==0 then return nil end
+	local retval = list.first.ref
+	local newfirst = list.first.next
+	list.first.next = nil
+	list.first = newfirst
+	if newfirst then newfirst.prev = nil end
+	list.n = list.n - 1
+	return retval
+end
+
+function List.popBack(list)
+	if list.n==0 then return nil end
+	local retval = list.last.ref
+	local newlast = list.last.prev
+	list.last.prev = nil
+	list.last = newlast
+	if newlast then newlast.next = nil end
+	list.n = list.n - 1
+	return retval
+end
+
+-- read functions
+function List.getFirst(list)
+	if list.n==0 then return nil end
+	list.current = list.first
+	return list.current.ref
+end
+
+function List.getNext(list)
+	if not list.current.next then return nil end
+	list.current = list.current.next
+	return list.current.ref
+end
+
+function List.getPrev(list)
+	if not list.current.prev then return nil end
+	list.current = list.current.prev
+	return list.current.ref
+end
+
+function List.getLast(list)
+	if list.n==0 then return nil end
+	list.current = list.last
+	return list.current.ref
+end
+
+function List.getCurrent(list)
+	return list.current.ref
+end
+
+-- remove current (use in combination with get*)
+function List.removeCurrent(list)
+	if list.n == 0 then return end
+	if list.n == 1 then
+		List.discard(list)
+		return
+	end
+
+	if list.current.next and list.current.prev then
+		list.current.next.prev = list.current.prev
+		list.current.prev.next = list.current.next
+--~ 		list.current = list.current.next
+		list.current.ref = nil
+		list.n = list.n - 1
+		return
+	end
+
+	-- first
+	if list.current == list.first then
+		list.first = list.first.next
+		list.first.prev = nil
+		list.current.next = nil
+--~ 		list.current = list.first
+		list.current.ref = nil
+		list.n = list.n - 1
+		return
+	end
+
+	-- last
+	if list.current == list.last then
+		list.last = list.last.prev
+		list.last.next = nil
+		list.current.prev = nil
+--~ 		list.current = list.last
+		list.current.ref = nil
+		list.n = list.n - 1
+		return
+	end
+end
+
+-- insert after current
+function List.insert(list, obj, value)
+	if not value then value=0 end
+
+	if list.n<2 or list.current==list.last then
+		List.pushBack(list,obj,value)
+		return
+	end
+
+	local new = { next = list.current.next, prev = list.current, ref = obj, val = value }
+	list.current.next = new
+	new.next.prev = new
+	list.n = list.n + 1
+end
+
+--- sorting
+function List.pushFrontSorted(list, obj, value)
+	if not value then value=0 end
+	if list.n==0 then
+		List.pushFront(list,obj,value)
+		return
+	end
+	if list.first.val > value then
+		List.pushFront(list,obj,value)
+		return
+	end
+
+	local elem = List.getFirst( list )
+	while elem and list.current.val < value do elem=List.getNext(list) end
+	if not elem then
+		List.pushBack(list,obj,value)
+	else
+		List.getPrev(list)
+		List.insert(list,obj,value)
+	end
+end
+
+function List.pushBackSorted(list, obj, value)
+	if not value then value=0 end
+	if list.n == 0 then
+		List.pushBack(list,obj,value)
+		return
+	end
+	if list.last.val < value then
+		List.pushBack(list,obj,value)
+		return
+	end
+
+	local elem = List.getLast( list )
+	while elem and list.current.val > value do elem=List.getPrev(list) end
+	if not elem then
+		List.pushFront(list,obj,value)
+	else
+		List.insert(list,obj,value)
+	end
+end
+
+-- changes the value of the current object and sorts again
+function List.changeValue(list, value)
+	if not value then value=0 end
+	if list.current.val == value then return end
+
+	local savedref = list.current.ref
+	local oldval = list.current.val
+	local newval = value
+	local oldprev = list.current.prev
+
+	List.removeCurrent(list)
+
+	if newval>oldval then
+		-- move up
+		elem = List.getNext(list)
+		while elem and list.current.val < value do elem = List.getNext(list) end
+		if not elem then
+			List.pushBack(list,savedref,value)
+		else
+			List.getPrev(list)
+			List.insert(list,savedref,value)
+		end
+	else
+		-- move down
+		local elem = List.getPrev( list )
+		while elem and list.current.val > value do elem=List.getPrev(list) end
+		if not elem then
+			List.pushFront(list,savedref,value)
+		else
+			List.insert(list,savedref,value)
+		end
+	end
+
+	-- not sure about this...
+	list.current = oldprev
+end
+
+--~ -- Example of use
+--~ list = List.newList()
+--~ List.pushBack(list,"abc")
+--~ List.pushBack(list,"cde")
+--~ List.pushBack(list,"def")
+
+--~ -- print
+--~ elem = List.getFirst(list)
+--~ while elem do
+--~ 	print(elem)
+--~ 	elem = List.getNext(list)
+--~ end
+
+--~ -- reset
+--~ print()
+--~ List.discard(list)
+
+--~ -- sort while inserting
+--~ List.pushFrontSorted(list,"iii",3)
+--~ List.pushFrontSorted(list,"ooo",4)
+--~ List.pushFrontSorted(list,"aaa",1)
+
+--~ -- print in reverse order (from high to low)
+--~ elem = List.getLast(list)
+--~ while elem do
+--~ 	print(elem)
+--~ 	elem = List.getPrev(list)
+--~ end
---
+++ scheduler.lua
@@ -0,0 +1,285 @@
+
+-- Blind Tower Defense (temporal name)
+-- Copyright 2009 Iwan Gabovitch, Christiaan Janssen, September 2009
+--
+-- This file is part of Blind Tower Defense
+--
+--     Blind Tower Defense is free software: you can redistribute it and/or modify
+--     it under the terms of the GNU General Public License as published by
+--     the Free Software Foundation, either version 3 of the License, or
+--     (at your option) any later version.
+--
+--     Blind Tower Defense is distributed in the hope that it will be useful,
+--     but WITHOUT ANY WARRANTY; without even the implied warranty of
+--     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--     GNU General Public License for more details.
+--
+--     You should have received a copy of the GNU General Public License
+--     along with Blind Tower Defense  If not, see <http://www.gnu.org/licenses/>.
+
+if not love then
+	--love.timer.getTime()
+	love = { timer = { } }
+	function love.timer.getTime()
+		return os.clock()
+	end
+ 	dofile("linkedlists.lua")
+end
+
+Scheduler = {}
+Tasks = {}
+
+Tasks.types = {
+	urgent = 1,
+	timed = 2,
+	untimed = 3,
+}
+
+function Tasks.create( visitorInstance, ttype, iterationPeriod )
+	if not iterationPeriod then iterationPeriod = 10000 end -- large
+	local task={
+		active = true,
+		estimated_delay = 0,
+		finished = false,
+		visitor = visitorInstance,
+		ready = false,
+		time_alpha = 0.9,
+		tasktype = ttype,
+		period = iterationPeriod,
+		timer = 0,
+		dt = 0,
+	}
+	task.stopwatch = love.timer.getTime()
+	return task
+end
+
+function Tasks.iteration(max_delay, task)
+	if not task.ready then
+		task.visitor.reset_loop()
+		task.finished = false
+		task.ready = true
+		task.active = true
+	end
+
+	local elapsed=0
+	local starttime = love.timer.getTime()
+	local startwatch = starttime
+	while (elapsed + task.estimated_delay < max_delay) and (not task.finished) do
+
+		task.finished = task.visitor.iterate(task.dt)
+
+		local stopwatch = love.timer.getTime()
+		task.estimated_delay = task.time_alpha * task.estimated_delay + (1-task.time_alpha)*(stopwatch-startwatch)
+		elapsed = stopwatch-starttime
+		startwatch = stopwatch
+	end
+	if task.estimated_delay >= max_delay then
+		-- timed out!
+		task.estimated_delay = task.estimated_delay * task.time_alpha
+	end
+
+	if task.finished then
+		task.visitor.finish_loop()
+		task.active = false
+	end
+end
+
+function Tasks.checkActive(task)
+	local stopwatch = love.timer.getTime()
+	task.dt = stopwatch - task.stopwatch
+	task.stopwatch = stopwatch
+
+	if task.tasktype == Tasks.types.timed then
+		task.timer = task.timer - task.dt
+		if task.timer <= 0 then
+			if task.ready and not task.finished then print("Time Out!") end
+			task.active = true
+			task.ready = false
+			task.timer = task.period
+		end
+	end
+	return task.active
+end
+
+
+--- Scheduler
+function Scheduler.newScheduler()
+	local scheduler = {}
+	scheduler.urgentTasks = List.newList()
+	scheduler.timedTasks = List.newList()
+	scheduler.untimedTasks = List.newList()
+	scheduler.sleepingTasks = List.newList()
+	return scheduler
+end
+
+function Scheduler.addTimedTask(scheduler, visitor, period)
+	--scheduler.task = task
+	local newtask = Tasks.create( visitor, Tasks.types.timed, period)
+	List.pushBack(scheduler.sleepingTasks,newtask)
+end
+
+function Scheduler.addUntimedTask(scheduler, visitor)
+	local newtask = Tasks.create( visitor, Tasks.types.untimed)
+	List.pushBack(scheduler.untimedTasks, newtask)
+end
+
+function Scheduler.addUrgentTask(scheduler, visitor)
+	local newtask = Tasks.create( visitor, Tasks.types.urgent)
+	List.pushBack(scheduler.urgentTasks, newtask)
+end
+
+function Scheduler.iteration(scheduler, max_delay)
+	local startclock = love.timer.getTime()
+	local task
+
+	-- Urgent tasks (must be finished now!)
+	while scheduler.urgentTasks.n>0 do
+		task = List.getFirst(scheduler.urgentTasks)
+		local time_slot = (max_delay - (love.timer.getTime() - startclock))/scheduler.urgentTasks.n
+		if time_slot < 0 then time_slot = max_delay end
+		while task do
+			if Tasks.checkActive(task) then
+				Tasks.iteration(time_slot,task)
+			else
+				List.removeCurrent(scheduler.urgentTasks)
+			end
+			task = List.getNext(scheduler.urgentTasks)
+		end
+	end
+
+	-- wake up sleeping tasks
+	task = List.getFirst(scheduler.sleepingTasks)
+	while task do
+		if Tasks.checkActive(task) then
+			List.pushBack(scheduler.timedTasks, task)
+			List.removeCurrent(scheduler.sleepingTasks)
+		end
+		task = List.getNext(scheduler.sleepingTasks)
+	end
+
+	-- Timed tasks
+	local taskcount = scheduler.timedTasks.n + scheduler.untimedTasks.n
+	if scheduler.timedTasks.n > 0 then
+		task = List.getFirst(scheduler.timedTasks)
+		while task do
+			local available_time = max_delay - (love.timer.getTime() - startclock)
+			local time_slot = available_time / taskcount
+
+			if Tasks.checkActive(task) then
+				Tasks.iteration(time_slot, task)
+			else
+				List.pushBack(scheduler.sleepingTasks, task)
+				List.removeCurrent(scheduler.timedTasks)
+			end
+			task = List.getNext(scheduler.timedTasks)
+			taskcount = taskcount - 1
+		end
+	end
+
+	-- Untimed tasks
+	if scheduler.untimedTasks.n > 0 then
+		task = List.getFirst(scheduler.untimedTasks)
+		while task do
+			local available_time = max_delay - (love.timer.getTime() - startclock)
+			local time_slot = available_time / taskcount
+
+			if Tasks.checkActive(task) then
+				Tasks.iteration(time_slot, task)
+			else
+				List.removeCurrent(scheduler.untimedTasks)
+			end
+			task = List.getNext(scheduler.untimedTasks)
+			taskcount = taskcount - 1
+		end
+	end
+end
+
+
+----------------------------------------------------
+-- Example of usage
+---------------------------------------------------------
+
+--- run test
+--~ Visitor={}
+--~ function Visitor.reset_loop()
+--~ 	Visitor.i = 0
+--~ 	Visitor.lim = 10
+--~ 	Visitor.j = Visitor.j + 1
+--~ end
+
+--~ function Visitor.iterate(dt)
+--~ 	local i,j
+--~ 	local A={}
+--~ 	local M=100
+--~ 	for i=1,M do
+--~ 		A[i]={}
+--~ 		for j=1,M do
+--~ 			A[i][M+1-j] = "chiska"
+--~ 		end
+--~ 	end
+
+--~ 	Visitor.i = Visitor.i + 1
+--~ 	print(Visitor.j.." a "..Visitor.i)
+--~ 	if Visitor.i==Visitor.lim then return true end
+--~ 	return false
+--~ end
+
+--~ function Visitor.finish_loop()
+--~ end
+
+--~ Visitor.j = 0
+
+--~ Visitor2={}
+--~ function Visitor2.reset_loop()
+--~ 	Visitor2.i = 0
+--~ 	Visitor2.lim = 20
+--~ end
+
+--~ function Visitor2.iterate(dt)
+--~ 	local i,j
+--~ 	local A={}
+--~ 	local M=200
+--~ 	for i=1,M do
+--~ 		A[i]={}
+--~ 		for j=1,M do
+--~ 			A[i][M+1-j] = "chiska"
+--~ 		end
+--~ 	end
+
+--~ 	Visitor2.i = Visitor2.i + 1
+--~ 	print(" b "..Visitor2.i)
+--~ 	if Visitor2.i==Visitor2.lim then return true end
+--~ 	return false
+--~ end
+
+--~ function Visitor2.finish_loop()
+--~ end
+
+--~ Visitor3={}
+--~ function Visitor3.reset_loop()
+--~ 	Visitor3.i = 0
+--~ 	Visitor3.lim = 100
+--~ end
+
+--~ function Visitor3.iterate(dt)
+
+--~ 	Visitor3.i = Visitor3.i + 1
+--~ 	print(" c "..Visitor3.i)
+--~ 	if Visitor3.i==Visitor3.lim then return true end
+--~ 	return false
+--~ end
+
+--~ function Visitor3.finish_loop()
+--~ end
+
+--~ sched = Scheduler.newScheduler()
+--~ Scheduler.addTimedTask(sched, Visitor, 1)
+--~ Scheduler.addUntimedTask(sched, Visitor2)
+--~ Scheduler.addUrgentTask(sched, Visitor3)
+
+--~ local totallimit = 1000
+--~ while Visitor.j<10 do
+--~ 	Scheduler.iteration(sched, 0.1)
+--~ end
+
+
