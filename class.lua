961ba2b0434b166946d349dad52539c4c3a021da
ChrisJan00 <Christiaan@.(none)>
2010-02-21 15:22:49 +0100

    OOP in lua

---
+++ class.lua
@@ -0,0 +1,90 @@
+-- Blind Tower Defense (temporal name)
+-- Copyright 2009 Iwan Gabovitch, Christiaan Janssen, September 2009
+--
+-- This file is part of Blind Tower Defense
+--
+--     Blind Tower Defense is free software: you can redistribute it and/or modify
+--     it under the terms of the GNU General Public License as published by
+--     the Free Software Foundation, either version 3 of the License, or
+--     (at your option) any later version.
+--
+--     Blind Tower Defense is distributed in the hope that it will be useful,
+--     but WITHOUT ANY WARRANTY; without even the implied warranty of
+--     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--     GNU General Public License for more details.
+--
+--     You should have received a copy of the GNU General Public License
+--     along with Blind Tower Defense  If not, see <http://www.gnu.org/licenses/>.
+
+-- class.lua
+-- Compatible with Lua 5.1 (not 5.0).
+function class(base, init)
+   local c = {}    -- a new class instance
+   if not init and type(base) == 'function' then
+      init = base
+      base = nil
+   elseif type(base) == 'table' then
+    -- our new class is a shallow copy of the base class!
+      for i,v in pairs(base) do
+         c[i] = v
+      end
+      c._base = base
+   end
+   -- the class will be the metatable for all its objects,
+   -- and they will look up their methods in it.
+   c.__index = c
+
+   -- expose a constructor which can be called by <classname>(<args>)
+   local mt = {}
+   mt.__call = function(class_tbl, ...)
+   local obj = {}
+   setmetatable(obj,c)
+   if init then
+      init(obj,...)
+   else
+      -- make sure that any stuff from the base class is initialized!
+      if base and base.init then
+      base.init(obj, ...)
+      end
+   end
+   return obj
+   end
+   c.init = init
+   c.is_a = function(self, klass)
+      local m = getmetatable(self)
+      while m do
+         if m == klass then return true end
+         m = m._base
+      end
+      return false
+   end
+   setmetatable(c, mt)
+   return c
+end
+
+--~ A = class( function(a,i)
+--~ 	a.i=i or 10
+--~ 	end )
+
+--~ function A:inc()
+--~ 	self.i = self.i+1
+--~ end
+
+--~ function A:show()
+--~ 	print(self.i)
+--~ end
+
+--~ B = class(A)
+--~ function B:inc()
+--~ 	self.i = self.i+20
+--~ end
+
+--~ inst = A()
+--~ inst:show()
+--~ inst:inc()
+--~ inst:show()
+
+--~ inst2 = B()
+--~ inst:show()
+--~ inst:inc()
+--~ inst:show()
